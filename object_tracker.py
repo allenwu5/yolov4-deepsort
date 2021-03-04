ab114b38328b42943f7c53250d15609a8f5d988f
Allen Wu <arashilen@gmail.com>
2021-03-04 12:19:31 +0800

    force to use gpu

--- object_tracker.py
+++ object_tracker.py
@@ -1,11 +1,10 @@
 import os
-# comment out below line to enable tensorflow logging outputs
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import time
 import tensorflow as tf
 physical_devices = tf.config.experimental.list_physical_devices('GPU')
-if len(physical_devices) > 0:
-    tf.config.experimental.set_memory_growth(physical_devices[0], True)
+# force to use GPU
+assert len(physical_devices) > 0
+tf.config.experimental.set_memory_growth(physical_devices[0], True)
 from absl import app, flags, logging
 from absl.flags import FLAGS
 import core.utils as utils
